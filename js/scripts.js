5a98457ef00fa9ec48175a7e8e5d446f34d4d69f
Slavcho Mitrov <slavemitrov@yahoo.com>
2017-05-03 21:15:54 +0200

    Add business and user interface logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,26 @@
+// Business (or back-end) logic:
+
 var add = function(number1, number2) {
 	return number1 + number2;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:");
-var result = add(number1, number2);
-alert(result);
+var subtract = function(number1, number2) {
+	return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+	return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+	return number1 / number2;
+};
+
+// Everything below this line is user interface (or front-end) logic:
+
+$(document).ready(function() {
+	var number1 = parseInt(prompt("Enter a number:"));
+	var number2 = parseInt(prompt("Enter another number"));
+
+	alert(add(number1, number2));
+});
